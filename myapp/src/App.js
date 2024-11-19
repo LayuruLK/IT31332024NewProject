39231dc5d569ac410f336e5b0acf76f20b102f70
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2024-11-19 08:41:27 +0000

    Import Database

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Students } from './StudentsDb';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>Student Information Portal</h1>
     </div>
   );
 }
